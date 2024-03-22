4dd94e2f117e1b061fb651ac39efd54fa32dc068
monkey-d-16 <silisio16@mail.ru>
2024-03-22 20:23:39 +0300

    6hqmework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { latLng, tileLayer } from 'leaflet';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, RouterOutlet} from '@angular/router';
+import {LeafletDirective, LeafletModule} from "@asymmetrik/ngx-leaflet";
+import {latLng, tileLayer} from "leaflet";
+import {GeocodingService} from "./services/geocoding.service";
 
 @Component({
   selector: 'app-root',
@@ -11,20 +12,36 @@ import { latLng, tileLayer } from 'leaflet';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'kubsu-4MM-map';
+export class AppComponent implements OnInit {
 
-  options = {
-    layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-    ],
-    zoom: 15,
-    center: latLng(45.019487, 39.031094)
-  };
+  @ViewChild(LeafletDirective)
+  protected leaflet!: LeafletDirective;
 
-
-  onMapReady(map: L.Map): void{
-    console.log(map);
-    
+  public constructor(private readonly route: ActivatedRoute,
+                     private readonly geocodingService: GeocodingService) {
   }
+
+  public ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      const search = params['search'];
+      if (search !== undefined) {
+        this.geocodingService.search(search)
+          .subscribe(coordinates => {
+            this.leaflet.getMap().flyTo(coordinates, 17);
+          });
+      }
+    });
+  }
+
+
+
+  protected options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 18, attribution: '...'})
+    ],
+    zoom: 17,
+    center: latLng(45.646266,42.956907)
+  };
+  
+
 }
